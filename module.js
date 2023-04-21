0c43742daaa1ed9210f5c4186d54dc9ae8cf651d
partybush1 <jasonclark1999@yahoo.com>
2023-04-21 03:40:04 -0500

    styling changes: added waves added navbar
    Testing Js

---
+++ module.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const header = document.querySelector("navbar");
+const main = document.querySelector("main");
+const headerHeight = document.querySelector("navbar").offsetHeight;
+main.style.top = headerHeight + "px";
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+  if (currentScroll - lastScroll > 0) {
+    header.classList.add("scroll-down");
+    header.classList.remove("scroll-up");
+  } else {
+    // scrolled up -- header show
+    header.classList.add("scroll-up");
+    header.classList.remove("scroll-down");
+  }
+  lastScroll = currentScroll;
+})
