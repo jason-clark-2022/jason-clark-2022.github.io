eb26b78e4dd385e19a1b6719194102825b1a3b96
partybush1 <jasonclark1999@yahoo.com>
2023-04-25 21:49:04 -0500

    page expansion added bio and resume sections added parralax for bio section

--- module.js
+++ module.js
@@ -23,3 +23,32 @@ window.addEventListener("scroll", () => {
   lastScroll = currentScroll;
 })
 
+
+
+
+// let title = document.querySelector('div.title');
+
+// document.addEventListener('mousemove', function(event) {
+//   let mouseX = event.clientX;
+//   let mouseY = event.clientY;
+
+//   let titleX = title.getBoundingClientRect().left + (title.offsetWidth / 3);
+//   let titleY = title.getBoundingClientRect().top + (title.offsetHeight / 3);
+
+//   let distanceX = mouseX - titleX;
+//   let distanceY = mouseY - titleY;
+
+//   let newTitleX = titleX - (distanceX / 80);
+//   let newTitleY = titleY - (distanceY / 80);
+
+//   title.style.transform = 'translate(' + (newTitleX - titleX) / 2 + 'px, ' + (newTitleY - titleY) / 2 + 'px)';
+// });
+
+
+
+
+window.addEventListener('scroll', function() {
+  const scrollPosition = window.scrollY;
+  const background = document.querySelector('.background');
+  background.style.transform = 'translate3d(0, ' + -scrollPosition / 5 + 'px, 0)';
+});
